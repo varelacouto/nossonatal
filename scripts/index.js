18be919aaf03cd1bbc3cd39e687c48bf6729a1f0
varelacouto <varela329@gmail.com>
2023-12-21 17:25:06 -0300

    uploada01

---
+++ scripts/index.js
@@ -0,0 +1,14 @@
+﻿const audioMerryChristmas = new Audio('../assets/audio/merry-christmas.mp3');
+const audioConfetti = new Audio('../assets/audio/confetti.wav');
+
+audioMerryChristmas.volume = 0.4;
+audioConfetti.volume = 0.2;
+
+setTimeout(function addItem() {
+
+    audioMerryChristmas.play();
+    audioConfetti.play();
+
+    
+    handleConfettiEffect() 
+}, 1000);
